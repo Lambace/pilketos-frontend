aa75c5a8878e11f9484782b288cdd1c447cca4fc
Lambace <97288921+Lambace@users.noreply.github.com>
2026-01-04 19:48:26 +0800

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,1 +1,25 @@
+const API_URL = "https://voting-backend-production-ea29.up.railway.app";
 
+export async function getStudents() {
+  const res = await fetch(`${API_URL}/students`);
+  return res.json();
+}
+
+export async function addStudent(data: any) {
+  const res = await fetch(`${API_URL}/students`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) throw new Error("Gagal menyimpan");
+  return res.json();
+}
+
+export async function getCandidates() {
+  const res = await fetch(`${API_URL}/candidates`);
+  return res.json();
+}
+
+export async function deleteStudent(nisn: string) {
+  return fetch(`${API_URL}/students/${nisn}`, { method: "DELETE" });
+}
